749175309e969530626b4a21083522e01801725a
ADennedy <90986187+ADennedy@users.noreply.github.com>
2021-10-19 21:50:11 -0700

    Update project2.js

--- project2.js
+++ project2.js
@@ -150,7 +150,238 @@ function draw() {
   }
 }
 
-  function touchStarted (){
+  function mousePressed (){
+    if (page==1){
+      if (mouseX<125 && mouseX>45 && mouseY<772 && mouseY>745){
+        page=2;
+      }
+      if (mouseX<340 && mouseX>135 && mouseY<772 && mouseY>745){
+        page=4;
+      }
+    }
+    else if (page==2){
+      if (mouseX<55 && mouseX>15 && mouseY<55 && mouseY>15){
+        page=1;
+      }
+      if (mouseX<360 && mouseX>30 && mouseY<780 && mouseY>732){
+        page=4;
+      }
+      if (mouseX<289 && mouseX>107 && mouseY<282 && mouseY>260){
+        page=3;
+      }
+      if (mouseX<335 && mouseX>55 && mouseY<545 && mouseY>300){
+        locY1=0;
+      }
+      else if (locY1==0 && mouseX<390 && mouseX>0 && mouseY<540 && mouseY>0){
+        locY1=844;
+      }
+    }
+    else if (page==3) {
+      if (mouseX<55 && mouseX>15 && mouseY<55 && mouseY>15){
+        page=1;
+      }
+      if (mouseX<360 && mouseX>30 && mouseY<780 && mouseY>732){
+        page=4;
+      }
+      if (mouseX<283 && mouseX>115 && mouseY<325 && mouseY>304){
+        page=2;
+      }
+      if (mouseX<335 && mouseX>55 && mouseY<487 && mouseY>340){
+        locY1=0;
+      }
+      else if (locY1==0 && mouseX<390 && mouseX>0 && mouseY<540 && mouseY>0){
+        locY1=844;
+      }
+    }
+    else if (page==4) {
+      if (mouseX<105 && mouseX>0 && mouseY<75 && mouseY>0){
+        menuOn= !menuOn;
+      }
+    }
+    else if (page==5) {
+      if (mouseX<105 && mouseX>0 && mouseY<75 && mouseY>0){
+        menuOn= !menuOn;
+      }
+      if (mouseX<386 && mouseX>322 && mouseY<545 && mouseY>485){
+        page=6;
+      }
+      if (mouseX<345 && mouseX>265 && mouseY<603 && mouseY>525){
+        page=7;
+      }
+      if (mouseX<315 && mouseX>210 && mouseY<560 && mouseY>360){
+        page=8;
+      }
+      if (mouseX<264 && mouseX>132 && mouseY<805 && mouseY>680){
+        page=9;
+      }
+      if (mouseX<375 && mouseX>280 && mouseY<795 && mouseY>705){
+        page=10;
+      }
+      if (mouseX<132 && mouseX>15 && mouseY<490 && mouseY>375){
+        page=11;
+      }
+      if (mouseX<160 && mouseX>37 && mouseY<650 && mouseY>540){
+        page=12;
+      }
+      if (mouseX<205 && mouseX>145 && mouseY<522 && mouseY>465){
+        page=13;
+      }
+    }
+    else if (page==6 || page==7 || page==8 || page==9 || page==10 || page==11 || page==12 || page==13){
+      if (mouseX<105 && mouseX>0 && mouseY<75 && mouseY>0){
+        menuOn= !menuOn;
+      }
+      if (mouseX<120 && mouseX>0 && mouseY<832 && mouseY>75){
+        page=5;
+      }
+      if (mouseX<386 && mouseX>120 && mouseY<832 && mouseY>695){
+        page=5;
+      }
+      if (mouseX<386 && mouseX>360 && mouseY<695 && mouseY>75){
+        page=5;
+      }
+      if (mouseX<360 && mouseX>120 && mouseY<225 && mouseY>75){
+        page=5;
+      }
+    }
+    else if (page==14) {
+      if (mouseX<105 && mouseX>0 && mouseY<75 && mouseY>0){
+        menuOn= !menuOn;
+      }
+    }
+    else if (page==15) {
+      if (mouseX<105 && mouseX>0 && mouseY<75 && mouseY>0){
+        menuOn= !menuOn;
+      }
+    }
+    else if (page==16) {
+      if (mouseX<105 && mouseX>0 && mouseY<75 && mouseY>0){
+        menuOn= !menuOn;
+      }
+    }
+    else if (page==17) {
+      if (mouseX<105 && mouseX>0 && mouseY<75 && mouseY>0){
+        menuOn= !menuOn;
+      }
+    }
+    else if (page==18) {
+      if (mouseX<105 && mouseX>0 && mouseY<75 && mouseY>0){
+        menuOn= !menuOn;
+      }
+      if (mouseX<380 && mouseX>350 && mouseY<580 && mouseY>550){
+        page=19;
+      }
+      if (mouseX<45 && mouseX>3 && mouseY<580 && mouseY>550){
+        page=21;
+      }
+      if (mouseX<263 && mouseX>125 && mouseY<820 && mouseY>790){
+        page=22;
+      }
+    }
+    else if (page==19) {
+      if (mouseX<105 && mouseX>0 && mouseY<75 && mouseY>0){
+        menuOn= !menuOn;
+      }
+      if (mouseX<380 && mouseX>350 && mouseY<580 && mouseY>550){
+        page=20;
+      }
+      if (mouseX<45 && mouseX>3 && mouseY<580 && mouseY>550){
+        page=18;
+      }
+      if (mouseX<263 && mouseX>125 && mouseY<820 && mouseY>790){
+        page=22;
+      }
+    }
+    else if (page==20) {
+      if (mouseX<105 && mouseX>0 && mouseY<75 && mouseY>0){
+        menuOn= !menuOn;
+      }
+      if (mouseX<380 && mouseX>350 && mouseY<580 && mouseY>550){
+        page=21;
+      }
+      if (mouseX<45 && mouseX>3 && mouseY<580 && mouseY>550){
+        page=19;
+      }
+      if (mouseX<263 && mouseX>125 && mouseY<820 && mouseY>790){
+        page=22;
+      }
+    }
+    else if (page==21) {
+      if (mouseX<105 && mouseX>0 && mouseY<75 && mouseY>0){
+        menuOn= !menuOn;
+      }
+      if (mouseX<380 && mouseX>350 && mouseY<580 && mouseY>550){
+        page=18;
+      }
+      if (mouseX<45 && mouseX>3 && mouseY<580 && mouseY>550){
+        page=20;
+      }
+      if (mouseX<263 && mouseX>125 && mouseY<820 && mouseY>790){
+        page=22;
+      }
+    }
+    else if (page==22) {
+      if (mouseX<105 && mouseX>0 && mouseY<75 && mouseY>0){
+        menuOn= !menuOn;
+      }
+      if (mouseX<262 && mouseX>128 && mouseY<777 && mouseY>745){
+        page=23;
+      }
+    }
+    else if (page==23) {
+      if (mouseX<105 && mouseX>0 && mouseY<75 && mouseY>0){
+        menuOn= !menuOn;
+      }
+      if (mouseX<277 && mouseX>112 && mouseY<812 && mouseY>762){
+        page=18;
+      }
+    }
+    else if (page==24) {
+      if (mouseX<105 && mouseX>0 && mouseY<75 && mouseY>0){
+        menuOn= !menuOn;
+      }
+      if (mouseX<390 && mouseX>0 && mouseY<844 && mouseY>800){
+        page=4;
+      }
+    }
+  //below is code for the navigation menu bar
+    if (menuOn==true) {
+      if (mouseX<280 && mouseX>19 && mouseY<155 && mouseY>85){
+        page=4;
+        menuOn= !menuOn;
+      }
+      if (mouseX<270 && mouseX>0 && mouseY<260 && mouseY>180){
+        page=5;
+        menuOn= !menuOn;
+      }
+      if (mouseX<280 && mouseX>19 && mouseY<365 && mouseY>265){
+        page=14;
+        menuOn= !menuOn;
+      }
+      if (mouseX<270 && mouseX>0 && mouseY<460 && mouseY>365){
+        page=15;
+        menuOn= !menuOn;
+      }
+      if (mouseX<280 && mouseX>19 && mouseY<555 && mouseY>460){
+        page=16;
+        menuOn= !menuOn;
+      }
+      if (mouseX<270 && mouseX>0 && mouseY<655 && mouseY>555){
+        page=17;
+        menuOn= !menuOn;
+      }
+      if (mouseX<280 && mouseX>19 && mouseY<742 && mouseY>655){
+        page=18;
+        menuOn= !menuOn;
+      }
+      if (mouseX<230 && mouseX>90 && mouseY<844 && mouseY>742){
+        page=24;
+        menuOn= !menuOn;
+      }
+    }
+  }
+
+function touchStarted (){
     if (page==1){
       if (mouseX<125 && mouseX>45 && mouseY<772 && mouseY>745){
         page=2;
